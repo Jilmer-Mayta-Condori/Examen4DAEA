c4413edc9a3a9aa20d33ac488974551944435152
Jilmer-Mayta-Condori <68447830+Jilmer-Mayta-Condori@users.noreply.github.com>
2021-12-02 10:00:26 -0500

    Examen terminado

--- Practica4/Examen4/Controllers/PrestamosController.cs
+++ Practica4/Examen4/Controllers/PrestamosController.cs
@@ -3,15 +3,134 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Examen4.Models;
+using System.Net;
+using System.Data;
+using System.IO;
+using System.Web.Helpers;
+using System.Collections;
 
 namespace Examen4.Controllers
 {
     public class PrestamosController : Controller
     {
-        // GET: Prestamos
+        #region Contexto
+        private LibraryEntities _contexto;
+        public LibraryEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new LibraryEntities();
+                return _contexto;
+            }
+        }
+        #endregion
         public ActionResult Index()
         {
+            return View(Contexto.Prestamos.ToList());
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Prestamos nuevoPrestamo)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Prestamos.Add(nuevoPrestamo);
+                    Contexto.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(nuevoPrestamo);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamos PrestamoEditar = Contexto.Prestamos.Find(id);
+            if (PrestamoEditar == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(PrestamoEditar);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Prestamos PrestamoEditar)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Entry(PrestamoEditar).State = System.Data.Entity.EntityState.Modified;
+                    Contexto.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(PrestamoEditar);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamos PrestamoEliminar = Contexto.Prestamos.Find(id);
+            if (PrestamoEliminar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(PrestamoEliminar);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id, Prestamos Categoria1)
+        {
+            try
+            {
+                Prestamos PrestamoEliminar = new Prestamos();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                    PrestamoEliminar = Contexto.Prestamos.Find(id);
+                    if (PrestamoEliminar == null)
+                    {
+                        return HttpNotFound();
+                    }
+                    Contexto.Prestamos.Remove(PrestamoEliminar);
+                    Contexto.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(PrestamoEliminar);
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
